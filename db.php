2eca7dea421ab23663750d8f0f2b9931b78a5f88
Karima-Muhammd <57095271+Karima-Muhammd@users.noreply.github.com>
2020-10-14 23:42:57 +0200

    Update db.php

--- db.php
+++ db.php
@@ -2,7 +2,7 @@
 $server_name="localhost";
 $user_name="root";
 $password="";
-$db_name="route";
+$db_name="moto_vehicle";
 
 $conn=mysqli_connect($server_name,$user_name,$password,$db_name);
 if(! $conn)
